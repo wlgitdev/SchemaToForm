3b9a2da234c820b191b2821af431f119bf7884d5
WL <wengellatham_dev@outlook.com>
2025-02-03 17:26:15 +0000

    Initialize UI Test App

--- packages/schema-to-form/jest.config.js
+++ packages/schema-to-form/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/src"],
-  testMatch: ["**/*.test.ts"],
+  testMatch: ["**/*.test.ts", "**/*.test.tsx"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
